0a35cef4af04ee314070102e00358292eb05fe44
Marion <marionroussel1@hotmail.fr>
2022-10-06 10:43:41 +0300

    Week 6, full stack tasks (until todo4)

---
+++ server/index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const cors = require('cors')
+const mysql = require('mysql2/promise')
+const config = require('./config')          // file and not a library
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+const port = 3001
+
+app.get("/", async (req, res) => {
+    try {
+    const connection = await mysql.createConnection(config.db)
+    const [result,] = await connection.execute('select * from task')
+
+    if (!result) result = [] // if there is no data, return empty array
+    res.status(200).json(result)
+
+    } catch(err) {
+        // Return status code 500 and error message to the client
+        res.status(500).json({error: err.message})
+    }
+})
+
+app.post("/new", async (req, res) => {
+    try {
+        const connection = await mysql.createConnection(config.db)
+        // Execute prepared statement
+        const [result,] = await connection.execute("insert into task (description) values (?) ", [req.body.description])
+        res.status(200).json({id:result.insertId})
+    } catch(err) {
+        res.status(500).json({error: err.message})
+    }
+})
+
+app.delete("/delete/:id", async (req, res) => {
+    try {
+        const connection = await mysql.createConnection(config.db)
+        // Execute prepared statement
+        await connection.execute("delete from task where id = ? ",[req.params.id])
+        res.status(200).json({id:req.params.id})
+    } catch(err) {
+        res.status(500).json({error:err.message})
+    }
+})
+
+app.listen(port)
